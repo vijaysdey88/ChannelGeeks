f9686acc61130a948888e27c029e1e8a7cae8c22
vijaysdey88 <vijaysdey88@gmail.com>
2015-05-28 18:35:58 +0530

    Login refactor to make async result simpler

--- AngularPractice/js/login.js
+++ AngularPractice/js/login.js
@@ -1,53 +1,27 @@
 app.controller("LoginController", function($scope, $location, LoginService){
 	var user = {};
 	$scope.user = user;
-	user.notAuthorized = false;
+	user.notAuthenticated = false;
 
 	$scope.login = function() {
 		console.log('Login user : ' + user.email + "/" + user.password);
 
-		LoginService.authenticate(user)
-					.then(function(response){
-							var isAuthenticated = response.data;
-							console.log('Result of authentication - ' + isAuthenticated);
-							if(isAuthenticated) {
-								user.notAuthorized = false;								
-								$location.path("/hello/" + user.email);
-							} else {
-								user.notAuthorized = true;
-							}
-					});
-
-		// if(LoginService.authenticate(user)) {
-		// 	user.notAuthorized = false;
-		// 	console.log("Login success :) ");
-		// 	$location.path("/hello/" + user.email);
-		// } else {
-		// 	user.notAuthorized = true;
-		// 	console.log("Login failure :( ");
-		// }
+		var onAuthSuccess = function(isAuthenticated){
+			console.log('Result of authentication - ' + isAuthenticated);
+			if(isAuthenticated) {
+				user.notAuthenticated = false;								
+				$location.path("/hello/" + user.email);
+			} else {
+				user.notAuthenticated = true;
+			}
+		}
+		LoginService.authenticate(user, onAuthSuccess);
 	};
 });
 
 app.service('LoginService', function($http) {
-
-	// $http.defaults.headers.put = {
- 	//        'Access-Control-Allow-Origin': '*',
- 	//        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
- 	//        'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With'
- 	//        };
-	// delete $http.defaults.headers.common['X-Requested-With'];
-	// $http.defaults.useXDomain = true;
-
-	this.isAuthorized = function(user) {
-		if("vd@c" === user.email && "vd" === user.password) {
-			return true;
-		} else {
-			return false;
-		}
-	};
-
-	this.authenticate = function(user) {
+	
+	this.authenticate = function(user, onAuthSuccess) {
 		var authRequest = {
 			method: "POST",
 			url: "http://localhost:8080/SpringMVC/rest/user/isvalid",
@@ -61,15 +35,14 @@ app.service('LoginService', function($http) {
 				password: user.password 
  			}
 		};
-
-		//$http.post("http://localhost:8080/SpringMVC/rest/user/isvalid", authRequest).
-		return $http(authRequest).
-					success(function(data, status, headers, config) {
-					    console.log('User authentication result - ' + data);
-					}).
-					error(function(data, status, headers, config) {
-					   	console.log('Failure while authenticating user - ' + data);
-					});
+		$http(authRequest)
+			.success(function(data, status, headers, config) {
+			    console.log('User authentication result - ' + data);
+			    onAuthSuccess(data);
+			})
+			.error(function(data, status, headers, config) {
+			   	console.log('Failure while authenticating user - ' + data);
+			})
 	}
 });
 
