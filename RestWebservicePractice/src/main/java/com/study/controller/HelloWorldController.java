12e3b9b5fc0b5371c73cba1ea76de7ba4050f761
vijaysdey88 <vijaysdey88@gmail.com>
2015-05-28 14:48:48 +0530

    Hello World ReST webservice.

--- RestWebservicePractice/src/main/java/com/study/controller/HelloWorldController.java
+++ RestWebservicePractice/src/main/java/com/study/controller/HelloWorldController.java
@@ -1,11 +1,15 @@
 package com.study.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class HelloWorldController {
 	
-	@RequestMapping("/hello")
-	public String hello() {
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
+	public @ResponseBody String hello() {
 		return "Hello!!!";
 	}
 
