0a2357e91ccf932e6cf4c0893f1df2319f9d2d7c
vijaysdey88 <vijaysdey88@gmail.com>
2015-07-05 17:18:44 +0530

    Ansync call to resolve message during routing

--- AngularPractice/js/demo_app.js
+++ AngularPractice/js/demo_app.js
@@ -1,60 +1,66 @@
 var app = angular.module('demoApp', ['ngRoute']);
 
-app.config(function($routeProvider){
+app.config(function($routeProvider) {
 	console.log('Inside Config!!!');
 	$routeProvider
 		.when("/", {
-			templateUrl : "template/main_page.html"
+			templateUrl: "template/main_page.html"
 		})
 		.when("/welcome/:userEmailId", {
-			controller: "WelcomeController",
-			template : "{{message}}",
+			controller: "UserWelcomeController",
+			template: "{{userWelcomeMessage}}",
 			resolve: {
-					message : function($route, WelcomeService) {
-						return WelcomeService.getWelcomeMessage($route.current.params.userEmailId);
-					}
+				"message": function($q, $timeout, WelcomeService) {
+					return WelcomeService.getWelcomeMessage();
+				}
 			}
 		});
 });
 
-app.factory("WelcomeService", function($http){
-	return {
-			getWelcomeMessage : function(userEmailId) {
-				return $http.get("http://localhost:8080/SpringMVC/rest/user/sayhello/" + userEmailId)
-                			.then(function(response){
-                				return response.data.message;
-                			})
-			}
-	};
-});
-
-app.directive("requiredField", function(){
-	return {
-		restrict : "E",
-		scope : {
-			name : "@",
-			type : "@",
-			model: "=",
-			placeholder : "@"
-		},
-		templateUrl : "../template/requiredField.html"
-	};
-});
-
-//Practice Directives
-app.controller("WelcomeController", function($scope, message){
-	$scope.message = "Welcome Message" + message + " !!!";
+app.controller("WelcomeController", function($scope) {
+	$scope.message = "Type in any message...";
 	$scope.logHello = function() {
 		console.log("Controller says Hello!!")
 	};
 });
 
-// Basic Behavious
-app.directive('messagePreview', function(){
+app.controller("UserWelcomeController", function($scope, message) {
+	$scope.userWelcomeMessage = "Message" + message + " !!!";
+});
+
+app.factory("WelcomeService", function($http, $route, $q) {
 	return {
-		restrict : "A",
-		template :  "<p>Preview {{message}}</p>",
-		link: function(scope, element, attributes){
+		getWelcomeMessage: function() {
+			var promise = $http.get("http://localhost:8080/SpringMVC/rest/user/sayhello/" + $route.current.params.userEmailId)
+				.then(function(response) {
+					console.log($route.current.params.userEmailId);
+					return response.data.message;
+				});
+			return promise;
+		}
+	};
+});
+
+app.directive("requiredField", function() {
+	return {
+		restrict: "E",
+		scope: {
+			name: "@",
+			type: "@",
+			model: "=",
+			placeholder: "@"
+		},
+		templateUrl: "../template/requiredField.html"
+	};
+});
+
+
+// Basic Behavious
+app.directive('messagePreview', function() {
+	return {
+		restrict: "A",
+		template: "<p>Preview {{message}}</p>",
+		link: function(scope, element, attributes) {
 			console.log('attributes', attributes.messagePreview);
 			element.css("color", attributes.messagePreview);
 		}
@@ -66,7 +72,7 @@ app.directive('enter', function() {
 		restrict: "A",
 		link: function(scope, element, attributes) {
 			element.bind("mouseenter", function() {
-				console.log("Content - " + element.text());				
+				console.log("Content - " + element.text());
 				scope.$apply(attributes.enter);
 			});
 		}
@@ -84,10 +90,10 @@ app.controller("CricketController", function($scope) {
 //Attribute bindings - @,==,&
 app.directive("cricketer", function() {
 	return {
-		scope :{
-			name : "@",
-			team : "=",
-			register : "&"
+		scope: {
+			name: "@",
+			team: "=",
+			register: "&"
 		},
 		restrict: "E",
 		templateUrl: "../template/cricketer.html"
@@ -112,7 +118,3 @@ app.directive('zippy', function() {
 		}
 	};
 });
-
-
-
-
--- RestWebservicePractice/src/main/java/com/study/controller/HelloWorldController.java
+++ RestWebservicePractice/src/main/java/com/study/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.study.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloWorldController {
 	
-	@RequestMapping(value="/hello", method=RequestMethod.GET)
+	@RequestMapping(value="/hello", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody String hello() {
 		return "Hello!!!";
 	}
